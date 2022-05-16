90a4e64da34e663924a4ab50ed9d9e18935493da
Maths Lover <redhacks@protonmail.com>
2022-05-16 13:16:35 +0530

    Update: Pull recent update from my repo for nvim

--- 3_nvim_setup.sh
+++ 3_nvim_setup.sh
@@ -8,6 +8,7 @@ DOTFILES_DIR=$(dirname "$0")
 pushd "$DOTFILES_DIR/AstroNvim_user" || return
 git remote add mehalterUpstream https://git.mehalter.com/mehalter/AstroNvim_user.git
 git pull mehalterUpstream --rebase
+git pull origin --rebase
 git switch maths-lover
 popd || exit
 rm -rfv "$HOME/.config/nvim/lua/user"
